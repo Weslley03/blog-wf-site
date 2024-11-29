53d011bd268bcb3567537d1537e69ba0fd486b04
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-29 16:38:43 -0300

    global styleds

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
   </main>
 </template>
 
-<script>
-
-</script>
-
 <style>
-
+  *{
+    background-color: hsl(0deg 0% 7.06%);
+    color: whitesmoke;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  }
 </style>
