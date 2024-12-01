43ff671048de636168324da880e96d7535010b29
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-30 23:03:04 -0300

    generic function to formValidation

---
+++ src/utils/formValidation.js
@@ -0,0 +1,24 @@
+function checkForm(data) {
+  const errors = {};
+
+  if(data.username) {
+    if(data.username.length < 3) errors.username = { message: 'o username deve ter no mínimo três caracteres' }
+  } else {
+    errors.username = { message: 'o campo não deve ser vazio' }
+  }
+  
+  if(data.email) {
+    if(data.email.length < 3) errors.email = { message: 'o e-mail deve ter no mínimo três caracteres' }
+  } else {
+    errors.email = { message: 'o campo não deve ser vazio' }
+  }
+
+  if(data.password) {
+    if(data.password.length < 3) errors.password = { message: 'a senha deve ter no mínimo três caracteres' }
+  } else{
+    errors.password = { message: 'o campo não deve ser vazio' }
+  }
+  return errors;
+};  
+
+export { checkForm };
