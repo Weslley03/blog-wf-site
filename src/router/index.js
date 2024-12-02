0b37286ff7a5b8d0e9c82b154af4ffe630f39be8
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-12-02 09:00:28 -0300

    implemented `authenticatedValidator`

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router";
+import { isAuthenticaded } from "../utils/authenticatedValidator";
 
 const routes = [
   { path: '/', name: 'Home',  component: () => import('../pages/HelloWorldPage.vue') },
-  { path: '/view-post/:idPost', name: 'ViewPost', component: () => import('../pages/HelloWorldPage.vue') },
+  { path: '/view-post/:idPost', name: 'ViewPost', beforeEnter: (to, from, next) => { isAuthenticaded(to, from, next) }, component: () => import('../pages/HelloWorldPage.vue') },
   { path: '/acesso', name: 'Acesso', component: () => import('../pages/AcessoPage.vue') },
   { path: '/cadastro', name: 'Cadastro', component: () => import('../pages/CadastroPage.vue') },
-  { path: '/user-profile/:idUser', name: 'UserProfile', component: () => import('../pages/HelloWorldPage.vue') },
-  { path: '/add-post/:idPost', name: 'AddPost', component: () => import('../pages/HelloWorldPage.vue') },
-  { path: '/edit-post/:idPost', name: 'EditPost', component: () => import('../pages/HelloWorldPage.vue') },
+  { path: '/user-profile/:idUser', name: 'UserProfile', beforeEnter: (to, from, next) => { isAuthenticaded(to, from, next) }, component: () => import('../pages/HelloWorldPage.vue') },
+  { path: '/add-post/:idPost', name: 'AddPost', beforeEnter: (to, from, next) => { isAuthenticaded(to, from, next) }, component: () => import('../pages/HelloWorldPage.vue') },
+  { path: '/edit-post/:idPost', name: 'EditPost', beforeEnter: (to, from, next) => { isAuthenticaded(to, from, next) }, component: () => import('../pages/HelloWorldPage.vue') },
   { path: '/:pathMatch(.*)*', name: 'ErroPage', component: () => import('../pages/HelloWorldPage.vue') }
 ]
 
