aba338bf47c5d27a256cc3609c7ee603ccc6c175
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-12-04 18:07:30 -0300

    added `NavUserName` component for Navbar

---
+++ src/components/NavUserName/NavUserName.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-user-name">
+    <span> {{ textName }} </span>    
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      textName: {
+        type: String,
+        require: true,
+      },
+    }
+  }
+</script>
+
+<style>
+  .nav-user-name {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+
+  span {
+    margin-left: auto;
+    text-align: end;
+    cursor: pointer;
+  }
+</style>  
