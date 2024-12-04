15ad5b306bff41c4780e03505db6c5d4886607f9
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-12-04 18:07:09 -0300

    added `NavUserComponent` for Navbar

---
+++ src/components/NavUserImage/NavUserImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img 
+      :src=srcImage
+    />    
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      srcImage: {
+        type: String,
+        require: true,
+      },
+    }
+  }
+</script>
+
+<style>
+  img{
+    cursor: pointer;
+    border-radius: 50%;
+    width: 3.5rem;
+    height: 3.5rem;
+  }
+</style>  
