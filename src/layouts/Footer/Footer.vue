c72ad278d4f066fea8ff6bd04585a24af3a655d0
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-12-06 17:30:52 -0300

    added footer for homepage

---
+++ src/layouts/Footer/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <a href="http:///weslleydev.com" target="_blank"> <span class="textSpan"> oweslley03 </span> </a>
+    <a href="https://github.com/weslley03" target="_blank"> <span class="textSpan"> github.com/weslley03 </span> </a>
+    <a href="https://www.linkedin.com/in/weslley-felipe-69a547187/" target="_blank"> <span class="textSpan"> linkedin.com/in/weslley-felipe </span> </a>
+  </footer>
+</template>
+
+<style>
+  .footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid whitesmoke;
+    border-radius: 10px;
+    padding: 10px;
+    margin: 10px
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .textSpan {
+    margin: 5px 0;
+    cursor: pointer
+  }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,33 +1,40 @@
 <template>
-  <div>
+  <div class="homepage">
     <Navbar />
     <h1 class="homePageTitle">Welcome!</h1>
     <div class="posts">
       <PostCard />
     </div>
   </div>
+  <Footer />
 </template>
 
 <script>
   import Navbar from '../layouts/Navbar/Navbar.vue';
   import PostCard from '../layouts/PostCard/PostCard.vue';
+  import Footer from '../layouts/Footer/Footer.vue';
 
   export default {
     components: {
       Navbar,
       PostCard,
+      Footer,
     }
   }
 </script> 
 
 <style>
+  .homepage {
+    min-height: 90vh;
+  }
+
   .homePageTitle {
     text-align: center;
   }
 
   .posts {
     display: grid;
-    grid-template-columns: repeat(4, auto) ;
+    grid-template-columns: repeat(4, auto);
     justify-content: center;
     align-items: center;
     gap: 1rem;
