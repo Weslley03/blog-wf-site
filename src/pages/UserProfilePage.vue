ee544969c0e703d451ac82ee9ac7e9953c861552
oweslley <wesleymoraeswv03@hotmail.com>
2024-12-28 19:22:41 -0300

    create `UserProfilePage`

---
+++ src/pages/UserProfilePage.vue
@@ -0,0 +1,48 @@
+<template>
+  <Navbar />
+  <div class="user_profile_main_content">
+    <h1>User Profile</h1>
+    <div>
+      <button class="user_profile_buttons"> edit profile</button>
+      <button class="user_profile_buttons"> add post</button>
+    </div>
+  </div>
+  <div class="user_profile_posts">
+    <PostCard :isProfilePage='true'/>
+  </div>
+</template>
+
+<script>
+  import Navbar from '../layouts/Navbar/Navbar.vue';
+  import PostCard from '../layouts/PostCard/PostCard.vue';
+
+  export default {
+    components: {
+      Navbar,
+      PostCard,
+    }
+  }
+</script>
+
+<style>
+  .user_profile_main_content {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  .user_profile_buttons {
+    margin: 5px;
+  }
+  
+  .user_profile_posts {
+    display: grid;
+    grid-template-columns: repeat(4, auto);
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    @media (max-width: 900px){
+      grid-template-columns: repeat(1, auto) ;
+    }
+  }
+</style>
