76a0ab93d2247d4a251680716e5eb20c86db8aa0
oweslley <wesleymoraeswv03@hotmail.com>
2025-01-10 22:34:28 -0300

    add `AddPostPage` with real structure

--- src/pages/AddPostPage.vue
+++ src/pages/AddPostPage.vue
@@ -1,3 +1,34 @@
 <template>
-<h1>Add Post</h1>
+  <Navbar />
+  <div class="add_post_main">
+    <div class="post_preview_section">
+      <PostCardPreview />
+    </div>
+  </div>
 </template>
+
+<script>
+  import Navbar from '../layouts/Navbar/Navbar.vue';
+  import PostCardPreview from '../layouts/PostCardPreview/PostCardPreview.vue';
+
+  export default {
+    components: {
+      Navbar,
+      PostCardPreview,
+    }
+  }
+</script>
+
+<style>
+  .add_post_main {
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .post_preview_section {
+    margin: 0 auto;
+    
+    height: 300px;
+    width: 90vw;
+  }
+</style>
