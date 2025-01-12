9739a5d0496bc745719d41abfed6ed9e1bec788b
oweslley <wesleymoraeswv03@hotmail.com>
2025-01-11 21:56:07 -0300

    add componentes para uso na tela de add post

--- src/pages/AddPostPage.vue
+++ src/pages/AddPostPage.vue
@@ -3,6 +3,14 @@
   <div class="add_post_main">
     <div class="post_preview_section">
       <PostCardPreview />
+      <div class="button_publish">
+        <FormButton :buttonWidth='100' :buttonHeight='30' buttonText='publicar' />
+      </div>
+      <div class="input_publish_post">
+        <FormInput id="input_post_image" v-model="post_image" inputType="file" inputPlaceholder="qual a imagem de capa?" />
+        <FormInput v-model="post_title" inputType="text" inputPlaceholder="qual o titulo?" />
+        <!-- <FormInput v-model="post_text" inputType="text" :inputHeight="400" inputPlaceholder="qual seu texto?" /> -->
+      </div>
     </div>
   </div>
 </template>
@@ -10,12 +18,25 @@
 <script>
   import Navbar from '../layouts/Navbar/Navbar.vue';
   import PostCardPreview from '../layouts/PostCardPreview/PostCardPreview.vue';
+  import FormButton from '../components/FormButton/FormButton.vue';
+  import FormInput from '../components/FormInput/FormInput.vue';
 
   export default {
     components: {
       Navbar,
       PostCardPreview,
+      FormButton,
+      FormInput,
+    },
+  
+    data() {
+      return {
+        post_image: '',
+        post_title: '',
+        post_text: '',
+      }
     }
+
   }
 </script>
 
